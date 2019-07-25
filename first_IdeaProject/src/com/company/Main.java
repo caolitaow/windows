56f811139fb1e8e535d83078cf5f4dffa477bf00
clt <1657227541@qq.com>
2019-07-25 20:12:28 +0800

    window下的代码

---
+++ first_IdeaProject/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+        public Main(){
+
+
+        }
+        public static  void main(String[] args) {
+                // write your code here
+                /*ic static
+                 * */
+                short s = 1;
+                s = (short)(s + 1);
+                s += 1;//不用强制类型转换
+                int 阿海=1;
+                //int goto=0;
+                //int const=0;
+                System.out.println(阿海);
+               // System.out.println(s);
+               // System.out.println("HelloWorld!");
+        }
+}
